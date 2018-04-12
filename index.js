07534bffe721d076d5493bd45d50d62c98144e45
Trish Brumett <trishbrumett@gmail.com>
2018-04-12 17:37:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   var lowercase = 'hello!'
   var uppercase = 'HELLO!'
-  if string === lowercase {
+  if (string === lowercase) {
       return "I can't hear you!"
-  } else if string === uppercase {
+  } else if (string === uppercase) {
       return "YES INDEED!"
   }
 
