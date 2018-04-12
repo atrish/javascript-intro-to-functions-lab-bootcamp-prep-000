a1d97af1509c005fc19fa178eed562c609c71501
Trish Brumett <trishbrumett@gmail.com>
2018-04-12 18:03:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
   var uppercase = "HELLO!"
       if (uppercase.toUpperCase() === uppercase) {
         return "YES INDEED!"
-      } else { {
+      } else { 
         return "I can't hear you!"
       }
 }
