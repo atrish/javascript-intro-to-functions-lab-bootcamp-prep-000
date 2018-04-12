efcc1292330b0cb998031b00e53eeccb54e72319
Trish Brumett <trishbrumett@gmail.com>
2018-04-12 18:00:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,15 @@ function logWhisper(string) {
     console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string, grandmother) {
-      return (${string} ${grandmother})
+function sayHiToGrandma(string) {
+  var lowercase = "hello"
+  var uppercase = "HELLO!"
+      if (uppercase.toUpperCase() === uppercase) {
+        return "YES INDEED!"
+      } else if (lowercase.toLowerCase() === lowercase) {
+        return "I can't hear you!"
+      }
 }
 
-sayHiToGrandma("hello" "I can't hear you")
-sayHiToGrandma("HELLO!" "YES INDEED!")
+sayHiToGrandma("hello")
+sayHiToGrandma("HELLO!")
