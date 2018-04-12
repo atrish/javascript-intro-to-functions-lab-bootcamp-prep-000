131235e7aec549b1b6851ac421c53b8648e8b858
Trish Brumett <trishbrumett@gmail.com>
2018-04-12 17:56:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,13 +14,9 @@ function logWhisper(string) {
     console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string) {
-
-  if (string.toLowerCase()) {
-      return "I can't hear you!"
-  } else if (string.toUpperCase) {
-      return "YES INDEED!"
-  }
+function sayHiToGrandma(string, grandmother) {
+      return ${string} ${grandmother} 
 }
 
-sayHiToGrandma('HELLO!')
+sayHiToGrandma("hello" "I can't hear you")
+sayHiToGrandma('HELLO!' "YES INDEED!")
